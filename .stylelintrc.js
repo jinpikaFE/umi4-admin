284f8a5b42acccdeb8094105e4fe249fa1d7b195
jinxin <jinxin@hsmap.com>
2022-07-08 16:44:49 +0800

    feat: lint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -4,5 +4,8 @@ module.exports = {
     'value-no-vendor-prefix': null,
     'declaration-block-no-redundant-longhand-properties': null,
     'no-empty-source': null,
+    'selector-class-pattern': null,
+    'custom-property-no-missing-var-function': null,
+    'function-no-unknown': null,
   },
 };
