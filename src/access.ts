1a78e6c7ca81343b60c56b9eca2c70822ffec5df
jinxin <jinxin@hsmap.com>
2022-07-29 17:46:59 +0800

    feat: 容错添加

--- src/access.ts
+++ src/access.ts
@@ -3,14 +3,11 @@ import { InitialStateType } from './app';
 export default (initialState: InitialStateType) => {
   // 在这里按照初始化数据定义项目中的权限，统一管理
   // 参考文档 https://next.umijs.org/docs/max/access
-  const { currentUser, authArr } = initialState;
-
-  console.log(authArr);
 
   return {
     normalRouteFilter: (route: { title: string }) => {
-      if (currentUser?.role?.[0]?.is_super) return true;
-      if (authArr?.includes(route.title)) {
+      if (initialState?.currentUser?.role?.[0]?.is_super) return true;
+      if (initialState?.authArr?.includes(route.title)) {
         return true;
       }
       return false;
