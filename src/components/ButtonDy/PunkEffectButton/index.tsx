b42d940a797e992b5b7996946d89e17061fedf59
jinxin <jinxin@hsmap.com>
2023-07-07 10:54:37 +0800

    feat: 特效按钮添加 antd

---
+++ src/components/ButtonDy/PunkEffectButton/index.tsx
@@ -0,0 +1,17 @@
+import { Button, ButtonProps } from 'antd';
+import React from 'react';
+import styles from './index.less';
+
+const PunkEffectButton: React.FC<
+  {
+    children: any;
+  } & ButtonProps
+> = ({ children, ...btnProps }) => {
+  return (
+    <div className={styles.container}>
+      <Button {...btnProps}>{children}</Button>
+    </div>
+  );
+};
+
+export default PunkEffectButton;
